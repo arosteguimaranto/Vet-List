65522ea188845025dc15a7ca1eec9936ba3bbec3
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-16 13:19:29 -0600

    Aplicando clases al Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Formulario } from "./components/Formulario"
-import Header from "./components/header"
+import {Header} from "./components/header"
 import { ListadoPacientes } from "./components/ListadoPacientes"
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
- function Header(){
+import React from 'react'
 
-    return(
-        <h1 > Desde Header</h1>
+export const Header = () => {
+    return (
+        <>
+            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto"
+            >
+             Seguimiento Pacientes 
+             <span className="text-indigo-600"> Veterinaria </span>
+             </h1>
+
+        </>
     )
 }
-
-export default Header;
-
