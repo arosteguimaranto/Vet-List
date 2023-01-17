76642f0dce4ee8759eaf5e66834ee73ed3abe117
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-17 13:09:56 -0600

    Añadir el resto de los campos en el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,8 +12,9 @@ export const Formulario = () => {
         <span className="text-indigo-600 font-bold text-lg">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
-        <div>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+
+        <div className="mb-5">
 
           <label htmlFor='mascota' className="block text-gray-700 uppercase
           font-bold"> Nombre Mascota
@@ -24,10 +25,73 @@ export const Formulario = () => {
             type="text"
             placeholder='Nombre de la Mascota'
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-
           />
-
         </div>
+
+
+        <div className="mb-5">
+
+          <label htmlFor='Propietario' className="block text-gray-700 uppercase
+        font-bold"> Nombre Propietario
+          </label>
+
+          <input
+            id="Propetario"
+            type="text"
+            placeholder='Nombre del Propietario'
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className='mb-5'>
+
+          <label htmlFor='Email' className="block text-gray-700 uppercase
+          font-bold"> Email
+          </label>
+
+          <input
+            id="Email"
+            type="email"
+            placeholder='Email contanto Propietario'
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+
+          <label htmlFor='Alta' className="block text-gray-700 uppercase
+          font-bold"> Alta
+          </label>
+
+          <input
+            id="Alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+
+          <label htmlFor='sintomas' className="block text-gray-700 uppercase
+          font-bold">
+            Sintomas
+          </label>
+
+          <textarea
+            id='sintomas'
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder='Describe los Sintomas'
+            />
+        </div>
+
+        <input 
+        type="submit"
+        className="bg-indigo-600 w-full p-3 text-white uppercase font-bold 
+        hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Agregar Paciente"
+
+        />
+
       </form>
 
     </div>
