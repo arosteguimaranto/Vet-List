679908959106d3857fb59a4b8dc0a5e64dd11f06
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-20 14:09:58 -0600

    Validación de Formularios en React

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,12 +7,20 @@ export const Formulario = () => {
   const [fecha, setFecha] = useState("");
   const [sintomas, setSintomas] = useState("");
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log('Enviando Formulario');
+
+    //Validacion de Formulario  
+    if ([nombre, propietario, email, fecha, sintomas].includes('')){
+      console.log('Hay almenos un campo vacio');
+    } else {
+      console.log('Todos llenos');
+    }
 
     
+
+
   }
 
   return (
@@ -23,9 +31,9 @@ export const Formulario = () => {
         <span className="text-indigo-600 font-bold text-lg">Administralos</span>
       </p>
 
-      <form 
-      onSubmit={handleSubmit}
-      className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
           <label
             htmlFor="mascota"
@@ -63,7 +71,7 @@ export const Formulario = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={propietario}
             onChange={(e) => setPropietario(e.target.value)}
-            />
+          />
         </div>
 
         <div className="mb-5">
@@ -83,7 +91,7 @@ export const Formulario = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            />
+          />
         </div>
 
         <div className="mb-5">
@@ -102,7 +110,7 @@ export const Formulario = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={fecha}
             onChange={(e) => setFecha(e.target.value)}
-            />
+          />
         </div>
 
         <div className="mb-5">
