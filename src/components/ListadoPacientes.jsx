dc1933ffc4e54ab595b1e6eee7bbda204e8c1b58
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-23 14:50:46 -0600

    Mostrando la informacion de los pacientes en el listado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -12,8 +12,9 @@ export const ListadoPacientes = ({pacientes}) => {
         <span className="text-indigo-600 font-bold"> Pacientes y Citas</span>
       </p>
     
-      {pacientes.map( (paciente) => (
+      {pacientes.map( (paciente, index) => (
           <Paciente
+          key={index}
           paciente={paciente}
           /> 
           
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,33 +1,36 @@
 
 
-export const Paciente = () => {
+export const Paciente = ({paciente}) => {
+
+  const{nombre, propietario, email, fecha, sintomas} =paciente
+
   return (
     <div className="mx-5 my-10  bg-white shadow-md px-5 py-10 rounded-xl">
     <p className="font-bold mb-3 text-gray-700 uppercase ">
       Nombre: {""}
-      <span className=" font-normal normal-case"> Hook </span>
+      <span className=" font-normal normal-case"> {nombre} </span>
     </p>
 
     <p className="font-bold mb-3 text-gray-700 uppercase ">
       Propietario: {""}
-      <span className=" font-normal normal-case"> Arostegui Maranto </span>
+      <span className=" font-normal normal-case"> {propietario} </span>
     </p>
 
     <p className="font-bold mb-3 text-gray-700 uppercase ">
       Email: {""}
-      <span className=" font-normal normal-case"> ositoPeru222Chupapis@HOTmail.com </span>
+      <span className=" font-normal normal-case"> {email} </span>
     </p>
 
     <p className="font-bold mb-3 text-gray-700 uppercase ">
       
       Fecha Alta: {""}
-      <span className=" font-normal normal-case"> un dia de estos </span>
+      <span className=" font-normal normal-case">{fecha} </span>
     </p>
 
     <p className="font-bold mb-3 text-gray-700 uppercase ">
       
       Sintomas: {""}
-      <span className=" font-normal normal-case"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam excepturi asperiores temporibus expedita, quae repellendus provident ipsa nesciunt vel dignissimos, explicabo dicta facere rerum consequuntur et pariatur voluptas ea! </span>
+      <span className=" font-normal normal-case"> {sintomas} </span>
     </p>
   </div>
   )
