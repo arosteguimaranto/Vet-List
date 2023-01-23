63d101c474dc63d373de8df12ebb58594eefb255
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-23 17:13:35 -0600

    Creando Botones de Editar y Eliminar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,37 +1,53 @@
 
 
-export const Paciente = ({paciente}) => {
+export const Paciente = ({ paciente }) => {
 
-  const{nombre, propietario, email, fecha, sintomas} =paciente
+  const { nombre, propietario, email, fecha, sintomas } = paciente
 
   return (
     <div className="mx-5 my-10  bg-white shadow-md px-5 py-10 rounded-xl">
-    <p className="font-bold mb-3 text-gray-700 uppercase ">
-      Nombre: {""}
-      <span className=" font-normal normal-case"> {nombre} </span>
-    </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Nombre: {""}
+        <span className=" font-normal normal-case"> {nombre} </span>
+      </p>
 
-    <p className="font-bold mb-3 text-gray-700 uppercase ">
-      Propietario: {""}
-      <span className=" font-normal normal-case"> {propietario} </span>
-    </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Propietario: {""}
+        <span className=" font-normal normal-case"> {propietario} </span>
+      </p>
 
-    <p className="font-bold mb-3 text-gray-700 uppercase ">
-      Email: {""}
-      <span className=" font-normal normal-case"> {email} </span>
-    </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Email: {""}
+        <span className=" font-normal normal-case"> {email} </span>
+      </p>
 
-    <p className="font-bold mb-3 text-gray-700 uppercase ">
-      
-      Fecha Alta: {""}
-      <span className=" font-normal normal-case">{fecha} </span>
-    </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
 
-    <p className="font-bold mb-3 text-gray-700 uppercase ">
-      
-      Sintomas: {""}
-      <span className=" font-normal normal-case"> {sintomas} </span>
-    </p>
-  </div>
+        Fecha Alta: {""}
+        <span className=" font-normal normal-case">{fecha} </span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+
+        Sintomas: {""}
+        <span className=" font-normal normal-case"> {sintomas} </span>
+      </p>
+
+      <div className="flex justify-between mt-10">
+        <button 
+        type="button"
+      className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold
+       uppercase rounded-lg"
+        >Editar
+        </button>
+
+        <button
+         type="button"
+         className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold
+         uppercase rounded-lg"
+         > Eliminar
+        </button>
+      </div>
+    </div>
   )
 }
