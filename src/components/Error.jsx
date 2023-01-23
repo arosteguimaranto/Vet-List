a97fa973a9dfe908b69fd4af7224ea26e10fae65
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-23 13:27:07 -0600

    Creando un componente de Errores

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const Error = ({mensaje}) => {
+  return (
+    <div className="bg-red-800
+    text-white text-center p-3
+   uppercase font-bold mb-3 rounded-md">
+     <p>{mensaje} </p>
+   </div>
+  )
+}
