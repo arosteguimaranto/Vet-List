b16091f60a3e0dca55191700c7653cbd3653d7e5
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-23 14:16:41 -0600

    El prop {children}

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,12 @@
 
 
 
-export const Error = ({mensaje}) => {
+export const Error = ({children}) => {
   return (
     <div className="bg-red-800
     text-white text-center p-3
    uppercase font-bold mb-3 rounded-md">
-     <p>{mensaje} </p>
+     {children} 
    </div>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -63,8 +63,8 @@ export const Formulario = ({pacientes, setPacientes }) => {
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
 
-        {error && <Error mensaje='Todos los campos son obligatorios'
-          /> }
+        {error && <Error> <p>Todos los campos son obligatorios</p> </Error>
+           }
         <div className="mb-5">
           <label
             htmlFor="mascota"
