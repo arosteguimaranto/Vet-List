a654c30760d36d042d1b288aa0b5b2453ab9e36e
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-25 18:44:42 -0600

    Agregando Local Storage para los pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Formulario } from "./components/Formulario"
 import { Header } from "./components/header"
 import { ListadoPacientes } from "./components/ListadoPacientes"
@@ -9,11 +9,18 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({})
 
+  useEffect(() => {
+   localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
+  
+
+
+
   const eliminarPaciente = id  =>{
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
     setPacientes(pacientesActualizados)
   }
-
+ 
 
 
   return (
